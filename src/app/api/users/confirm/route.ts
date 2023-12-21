cd816922e2c8a9920f10e036165cbc5d5ff2de6d
BiFoS <jon89071@gmail.com>
2023-12-21 09:26:51 +0900

    :arrow_up: Update next version

--- src/app/api/users/confirm/route.ts
+++ src/app/api/users/confirm/route.ts
@@ -1,7 +1,13 @@
+import { getIronSession } from "iron-session";
+import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   const { token } = await req.json();
-  console.log(token);
+
+  const session = await getIronSession(cookies(), {
+    password: "asdf",
+    cookieName: "NEXT_SESSION",
+  });
   return NextResponse.json({ ok: true }, { status: 200 });
 }
