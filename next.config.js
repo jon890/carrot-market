85bf385c5f59e0819122383a33c8c674862789f4
BiFoS <jon89071@gmail.com>
2024-04-02 16:41:56 +0900

    :lipstick: 유저 아바타 이미지 처리

--- next.config.js
+++ next.config.js
@@ -8,6 +8,13 @@ const nextConfig = {
         port: "",
         pathname: "**",
       },
+
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "**",
+      },
     ],
   },
 };
--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -58,7 +58,7 @@ export default async function ProductDetail({
       </div>
 
       <div className="flex items-center gap-3 border-b border-neutral-700 p-5">
-        <div className="size-10 rounded-full">
+        <div className="size-10 overflow-hidden rounded-full">
           {product.user.avatar ? (
             <Image
               src={product.user.avatar}
