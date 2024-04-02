0eaad044cd06346065765f2f73156aef0c1ddce4
BiFoS <jon89071@gmail.com>
2024-04-02 15:34:36 +0900

    :lipstick: 상품 스켈레톤 추가

--- src/app/(tabs)/products/page.tsx
+++ src/app/(tabs)/products/page.tsx
@@ -1,4 +1,12 @@
-export default function ProductsPage() {
+async function getProducts() {
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+
+  return [];
+}
+
+export default async function ProductsPage() {
+  const products = await getProducts();
+
   return (
     <div>
       <h1 className="text-4xl text-white"></h1>
