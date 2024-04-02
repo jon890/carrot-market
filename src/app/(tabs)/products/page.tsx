a0db76b9c4aa4e6ef67eff1cad4a7ac6aa4c4eee
BiFoS <jon89071@gmail.com>
2024-04-02 18:50:33 +0900

    :lipstick: 상품 추가 페이지 준비

--- src/app/(tabs)/products/page.tsx
+++ src/app/(tabs)/products/page.tsx
@@ -1,5 +1,7 @@
 import ProductList from "@/components/product-list";
 import client from "@/libs/prisma-client";
+import { PlusIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
 
 export async function getInitialProducts() {
   const products = await client.product.findMany({
@@ -25,6 +27,12 @@ export default async function ProductsPage() {
   return (
     <div>
       <ProductList initialProducts={initialProducts} />
+      <Link
+        href="/products/add"
+        className="fixed bottom-24 right-8 flex size-16 items-center justify-center rounded-full bg-orange-500 text-white transition-colors hover:bg-orange-400"
+      >
+        <PlusIcon className="size-10" />
+      </Link>
     </div>
   );
 }
---
+++ src/app/products/add/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import FormButton from "@/components/form-button";
+import Input from "@/components/input";
+import { PhotoIcon } from "@heroicons/react/24/solid";
+import { useState } from "react";
+
+export default function AddProduct() {
+  const [preview, setPreview] = useState("");
+  const onImageChange = () => {};
+  return (
+    <div>
+      <form className="flex flex-col gap-5 p-5">
+        <label
+          htmlFor="photo"
+          className="flex aspect-square cursor-pointer flex-col items-center justify-center rounded-md border-2 border-dashed border-neutral-300 text-neutral-300"
+        >
+          <PhotoIcon className="w-20" />
+          <div className="text-sm text-neutral-400">사진을 추가해주세요.</div>
+        </label>
+        <input
+          type="file"
+          id="photo"
+          name="photo"
+          className="hidden"
+          onChange={onImageChange}
+        />
+        <Input name="title" required placeholder="제목" type="text" />
+        <Input name="price" required placeholder="가격" type="text" />
+        <Input name="description" placeholder="자세한 설명" type="text" />
+        <FormButton text="작성 완료" />
+      </form>
+    </div>
+  );
+}
