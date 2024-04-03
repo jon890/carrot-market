c7bee394126676a426524a15bea3edd344251968
BiFoS <jon89071@gmail.com>
2024-04-03 10:15:49 +0900

    :lipstick: 상품 작성 페이지

---
+++ src/app/products/add/actions.ts
@@ -0,0 +1,67 @@
+"use server";
+
+import client from "@/libs/prisma-client";
+import { getSession } from "@/libs/session";
+import { redirect } from "next/navigation";
+import { File } from "node:buffer";
+import fs from "node:fs/promises";
+import { z } from "zod";
+
+const productSchema = z.object({
+  photo: z.string({
+    required_error: "Photo is required",
+  }),
+  title: z.string({
+    required_error: "Title is required",
+  }),
+  price: z.coerce.number({
+    required_error: "Price is required",
+  }),
+  description: z.string({
+    required_error: "Description is required",
+  }),
+});
+
+export async function uploadProduct(prevState: any, formData: FormData) {
+  const data = {
+    photo: formData.get("photo"),
+    title: formData.get("title"),
+    price: formData.get("price"),
+    description: formData.get("description"),
+  };
+
+  if (data.photo instanceof File) {
+    const photoData = await data.photo.arrayBuffer();
+    await fs.appendFile(`./public/${data.photo.name}`, Buffer.from(photoData));
+    data.photo = `/${data.photo.name}`;
+  }
+
+  const result = productSchema.safeParse(data);
+  if (!result.success) {
+    return result.error.flatten();
+  }
+
+  const session = await getSession();
+  if (!session.id) {
+    return null;
+  }
+
+  const product = await client.product.create({
+    data: {
+      title: result.data.title,
+      description: result.data.description,
+      price: result.data.price,
+      photo: result.data.photo,
+      user: {
+        connect: {
+          id: session.id,
+        },
+      },
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  redirect(`/products/${product.id}`);
+}
--- src/app/products/add/page.tsx
+++ src/app/products/add/page.tsx
@@ -4,30 +4,72 @@ import FormButton from "@/components/form-button";
 import Input from "@/components/input";
 import { PhotoIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
+import { uploadProduct } from "./actions";
+import { useFormState } from "react-dom";
 
 export default function AddProduct() {
   const [preview, setPreview] = useState("");
-  const onImageChange = () => {};
+  const [state, action] = useFormState(uploadProduct, null);
+
+  const onImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { files },
+    } = event;
+
+    const file = files?.[0];
+    if (!file) return;
+
+    setPreview(URL.createObjectURL(file));
+  };
+
   return (
     <div>
-      <form className="flex flex-col gap-5 p-5">
+      <form className="flex flex-col gap-5 p-5" action={action}>
         <label
           htmlFor="photo"
-          className="flex aspect-square cursor-pointer flex-col items-center justify-center rounded-md border-2 border-dashed border-neutral-300 text-neutral-300"
+          className="flex aspect-square cursor-pointer flex-col items-center justify-center rounded-md border-2 border-dashed border-neutral-300 bg-cover bg-center text-neutral-300"
+          style={{
+            backgroundImage: `url(${preview})`,
+          }}
         >
-          <PhotoIcon className="w-20" />
-          <div className="text-sm text-neutral-400">사진을 추가해주세요.</div>
+          {preview === "" && (
+            <>
+              <PhotoIcon className="w-20" />
+              <div className="text-sm text-neutral-400">
+                사진을 추가해주세요.
+                {state?.fieldErrors?.photo}
+              </div>
+            </>
+          )}
         </label>
         <input
           type="file"
           id="photo"
           name="photo"
           className="hidden"
+          accept="image/*"
           onChange={onImageChange}
         />
-        <Input name="title" required placeholder="제목" type="text" />
-        <Input name="price" required placeholder="가격" type="text" />
-        <Input name="description" placeholder="자세한 설명" type="text" />
+        <Input
+          name="title"
+          required
+          placeholder="제목"
+          type="text"
+          errors={state?.fieldErrors?.title}
+        />
+        <Input
+          name="price"
+          required
+          placeholder="가격"
+          type="text"
+          errors={state?.fieldErrors?.price}
+        />
+        <Input
+          name="description"
+          placeholder="자세한 설명"
+          type="text"
+          errors={state?.fieldErrors?.description}
+        />
         <FormButton text="작성 완료" />
       </form>
     </div>
